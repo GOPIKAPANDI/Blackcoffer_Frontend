8d60c1c05429717ab85a4fe8a4fdef0ea3a9ace2
GOPIKAPANDI <gopikapandi.r2021csea@sece.ac.in>
2024-01-16 20:18:59 +0530

    deploy_f

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {disableReactDevTools} from '@fvilers/disable-react-devtools'
 
  
 ReactDOM.createRoot(document.getElementById('root')).render(
